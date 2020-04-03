cb0bf29fa9c89871f0c16cb1f2c41a87cbeb5b81
Alexey Kolchin <murrel@yandex.ru>
2020-04-03 19:41:19 +0300

    2.5.14 version 1 final

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -111,7 +111,7 @@ AppAsset::register($this);
     <div class="container">
         <p class="float-left">&copy; My Company <?= date('Y') ?></p>
 
-        <p class="float-right"><?= Yii::powered() ?></p>
+        <p class="float-right">Сделано в <a href="https://yourwebstudio.ru">Вашей веб-студии</a></p>
     </div>
 </footer>
 
